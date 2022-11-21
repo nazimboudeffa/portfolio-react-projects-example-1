46c31cefcb1f66694b0da33bf3f6d2813e825d6a
nazimboudeffa <boudeffa@gmail.com>
2022-11-21 16:53:07 +0100

    First Commit

---
+++ chapter-2/src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import logo from './assets/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header logo={logo} />
+      <Profile userName="nazimboudeffa" />
+    </div>
+  );
+}
+
+export default App;
---
+++ chapter-2/src/components/Header.js
@@ -0,0 +1,17 @@
+import Link from './Link.js';
+import './Header.css';
+function Header({ logo }) {
+  return (
+    <header className='App-header'>
+      <img src={logo} className='App-logo' alt='logo'
+      />
+      <p>Edit <code>src/App.js</code> 
+        and save to reload. </p>
+      <Link 
+        url='https://reactjs.org'
+        title='Learn React' 
+      />
+    </header>
+  );
+}
+export default Header;
